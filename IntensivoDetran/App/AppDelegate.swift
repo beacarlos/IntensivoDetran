004b970cbfeccfd1bcf034fdf056e615b117f498
Beatriz Carlos <beatrizcarlos2000@gmail.com>
2021-02-03 10:17:26 -0300

    configuring the project folders.

--- IntensivoDetran/App/AppDelegate.swift
+++ IntensivoDetran/App/AppDelegate.swift
@@ -8,13 +8,16 @@
 import UIKit
 import CoreData
 
+//swiftlint:disable unused_closure_parameter
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = ViewController() // Your initial view controller.
+        window.makeKeyAndVisible()
+        self.window = window
         return true
     }
 
@@ -76,6 +79,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             }
         }
     }
-
 }
-
