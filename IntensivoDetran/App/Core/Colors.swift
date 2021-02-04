1feb8be47bcbc62af46d2dd8712e67f39f0be52e
Beatriz Carlos <beatrizcarlos2000@gmail.com>
2021-02-04 12:44:31 -0300

    extensions

---
+++ IntensivoDetran/App/Core/Colors.swift
@@ -0,0 +1,41 @@
+//
+//  Colors.swift
+//  IntensivoDetran
+//
+//  Created by Beatriz Carlos on 04/02/21.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    
+    static var colorPrimary: UIColor {
+        return UIColor(red: 0.00, green: 0.62, blue: 0.59, alpha: 1.00)
+    }
+    
+    static var colorPrimaryLight: UIColor {
+        return UIColor(red: 0.82, green: 0.90, blue: 0.89, alpha: 1.00)
+    }
+    
+    static var actionColor: UIColor {
+        return UIColor(red: 1.00, green: 0.43, blue: 0.05, alpha: 1.00)
+    }
+    
+    static var colorCaption: UIColor {
+        return UIColor(red: 0.44, green: 0.49, blue: 0.56, alpha: 1.00)
+    }
+    
+    static var colorTitle: UIColor {
+        return UIColor(red: 0.20, green: 0.21, blue: 0.43, alpha: 1.00)
+
+    }
+    
+    static var colorCheckedButton: UIColor {
+        return UIColor(red: 0.84, green: 0.95, blue: 0.93, alpha: 1.00)
+    }
+
+    static var whiteColor: UIColor {
+        return UIColor(red: 1.00, green: 1.00, blue: 1.00, alpha: 1.00)
+    }
+}
